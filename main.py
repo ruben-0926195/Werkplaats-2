175455e351914e986f9ddfcf52e76a34bdd456ea
ruben-0926195 <0926195@hr.nl>
2024-11-08 18:08:27 +0100

    Added database model and first template

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def overview():
-    return '<p>Hello World</p>'
+    message = "Hello, this is your Flask app's homepage!"
+    # This will render the index.html template and pass 'message' to it
+    return render_template('base.html', message=message)
 
 if __name__ == "__main__":
     app.run(debug=True)
