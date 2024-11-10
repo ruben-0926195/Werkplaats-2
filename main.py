4ecaaa88a2e8191cbd2681accf86a30a5ba1d411
ruben-0926195 <0926195@hr.nl>
2024-11-10 12:00:15 +0100

    Added user registration

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for, flash
 from models.user import User
 
 app = Flask(__name__)
@@ -7,9 +7,34 @@ app = Flask(__name__)
 def overview():
     data = User()
     users = data.get_all_users()
+
     # This will render the overview.html template and pass 'users' to it
     return render_template('overview.html', users=users)
 
+
+@app.route('/user/create', methods=['GET', 'POST'])
+def user_create():
+    if request.method == 'POST':
+        # Retrieve form data
+        login = request.form.get('login')
+        password = request.form.get('password')
+        display_name = request.form.get('display_name')
+        is_admin = request.form.get('is_admin') == 'on'  # Checkbox is checked
+
+        # Process the form data (save to database, etc.)
+        user_model = User()
+        new_user = user_model.create_user(login, password, display_name, is_admin)
+
+
+        # Redirect after successful form submission
+        if new_user:
+            print("User registered successfully!")
+            return redirect(url_for('overview'),)
+        else:
+            print("An error occurred. Please try again.")
+            return redirect(url_for('user_create'))
+
+    return render_template('user_create.html')
 
 
 if __name__ == "__main__":
--- models/user.py
+++ models/user.py
@@ -8,3 +8,10 @@ class User:
     def get_all_users(self):
         result = self.cursor.execute("SELECT * FROM users").fetchall()
         return result
+
+    def create_user(self, login, password, display_name, is_admin):
+        self.cursor.execute(
+            "INSERT into users (login,password,display_name,is_admin) VALUES (?,?,?,?)",
+            (login, password, display_name, is_admin))
+        self.con.commit()
+        return True
