16a48592925eb7e458d75a1baefd55832958c965
ruben-0926195 <0926195@hr.nl>
2024-12-04 12:22:02 +0100

    Changed overview page for questions

--- models/question.py
+++ models/question.py
@@ -6,9 +6,44 @@ class Questions:
         database = Database('./databases/database.db')
         self.cursor, self.con = database.connect_db()
 
-    def get_all_questions(self):
-        result = self.cursor.execute("SELECT * FROM questions").fetchall()
-        return result
+    # def get_all_questions(self):
+    #     result = self.cursor.execute("SELECT * FROM questions").fetchall()
+    #     return result
+
+    def get_all_questions(self, page, per_page, filters=None):
+
+        offset = (page - 1) * per_page
+
+        query_get_questions = "SELECT * FROM questions WHERE 1=1"
+        query_get_total_questions = "SELECT COUNT(*) FROM questions WHERE 1=1"
+        params = []
+
+        if filters:
+            # Apply filters
+            if filters.get("questions_id"):
+                query_get_questions += " AND questions_id LIKE ?"
+                query_get_total_questions += " AND questions_id LIKE ?"
+                params.append(f"%{filters['questions_id']}%")
+            if filters.get("question"):
+                query_get_questions += " AND question LIKE ?"
+                query_get_total_questions += " AND question LIKE ?"
+                params.append(f"%{filters['question']}%")
+            if filters.get("vak"):
+                query_get_questions += " AND vak LIKE ?"
+                query_get_total_questions += " AND vak LIKE ?"
+                params.append(f"%{filters['vak']}%")
+
+        query_get_questions += " LIMIT ? OFFSET ?"
+        params_get_questions = params + [per_page, offset]
+
+        # Execute the query
+        self.cursor.execute(query_get_questions, params_get_questions)
+        result = self.cursor.fetchall()
+
+        self.cursor.execute(query_get_total_questions, params)
+        total_questions = self.cursor.fetchone()[0]
+
+        return result, total_questions
 
     def get_single_question(self, question_id):
         self.cursor.execute("SELECT * FROM questions WHERE questions_id = ?", (question_id,))
