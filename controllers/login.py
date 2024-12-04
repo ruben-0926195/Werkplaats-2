5a59a1617e9b8100ba5b04e62069bb065a17a3da
Marco-1028839 <marco@marcodoornbos.nl>
2024-12-04 12:10:00 +0100

    + Added the login controller with all the functions + Added the the basic check

--- controllers/login.py
+++ controllers/login.py
@@ -1,13 +1,21 @@
 from flask import Blueprint, render_template, request, redirect, url_for, session
-from models.prompt import Prompt
+from models.user import User
 
 login_routes = Blueprint('login', __name__)
 
 @login_routes.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
+        username = request.form.get('username')
+        password = request.form.get('password')
+        user = User()
 
-def check_login(username, password):
-    user
+        username_check = user.check_user_in_db(username)
+        password_check = user.check_user_in_db(password)
+        if username_check and password_check:
+            return redirect(url_for('question.overview'))
+        else:
+            return render_template('login.html')
+
+    return render_template('login.html')
+
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ from flask import Flask
 from controllers.user import user_routes
 from controllers.question import question_routes
 from controllers.prompt import prompt_routes
+from controllers.login import login_routes
 
 app = Flask(__name__)
 app.secret_key = "your_secret_key"  # Replace with a strong secret key
 
+app.register_blueprint(login_routes)
 app.register_blueprint(user_routes)
 app.register_blueprint(question_routes)
 app.register_blueprint(prompt_routes)
